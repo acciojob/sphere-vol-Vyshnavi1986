4d31fc636c0169c6205843dc50f7df9ce5942479
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-27 11:23:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function calculateVolume() {
   // Get the radius input value
   var radius = document.getElementById("radius").value;
-
+if(radius<0)
+{
+	document.getElementbyId("result").innerHTML="The volume of the sphere is" +NAN;
+	return
+}
+	
   // Calculate the volume of the sphere
   var volume = (4/3) * Math.PI * Math.pow(radius, 3);
 
